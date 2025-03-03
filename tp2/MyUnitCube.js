236c5606d7f9aaf30b74d654ffe151d976eaba3f
up202205144 <up202205144@edu.pt>
2025-03-03 11:58:41 +0000

    UnitCube fix

--- tp2/MyUnitCube.js
+++ tp2/MyUnitCube.js
@@ -24,36 +24,23 @@ export class MyUnitCube extends CGFobject {
 
         this.indices = [
             0, 1, 2,
-            1, 0, 2,
             0, 2 ,3,
-            3, 2, 0,
 
-            4, 5, 6,
             5, 4, 6,
-            4, 6 ,7,
             7, 6, 4,
 
-            2, 3, 4,
             4, 3, 2,
-            2, 4, 7,
             7, 4, 2,
             
-            0, 1, 5,
             5, 1, 0,
             1, 5, 6,
-            6, 5, 1,
 
             6, 7, 1,
-            1, 7, 6,
             7, 2, 1,
-            1, 2, 7,
 
             4, 5, 3,
-            3, 5, 4,
-            5, 0, 3,
-            3, 0, 5
+            5, 0, 3
             
-
         ];
 
         this.primitiveType = this.scene.gl.TRIANGLES;
