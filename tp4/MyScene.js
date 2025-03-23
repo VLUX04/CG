3bf41917edfcfb9c9e2d9a35cb3aff371dfdf631
Granja5 <ze.diogo.granja@gmail.com>
2025-03-23 16:02:18 +0000

    quad checkbox

--- tp4/MyScene.js
+++ tp4/MyScene.js
@@ -46,6 +46,7 @@ export class MyScene extends CGFscene {
 
         //-------Objects connected to MyInterface
         this.displayAxis = true;
+        this.displayQuad = true;
         this.scaleFactor = 5;
         this.selectedTexture = -1;        
         this.wrapS = 0;
@@ -116,15 +117,16 @@ export class MyScene extends CGFscene {
 
         // ---- BEGIN Primitive drawing section
 
-        this.quadMaterial.apply();
+        if (this.displayQuad){
+            this.quadMaterial.apply(); 
+            this.quad.display();
+        }
 
         // Default texture filtering in WebCGF is LINEAR. 
         // Uncomment next line for NEAREST when magnifying, or 
         // add a checkbox in the GUI to alternate in real time
         
         // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
-
-        this.quad.display();
 
         // ---- END Primitive drawing section
     }
