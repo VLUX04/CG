d19ef14a21bcbc956a9923f099fdafa523e4cf54
Granja5 <ze.diogo.granja@gmail.com>
2025-03-25 23:23:34 +0000

    quad unit cube working correctly

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -12,51 +12,49 @@ export class MyUnitCubeQuad extends CGFobject {
         this.quad = new MyQuad(this.scene);
     }
 
-
     display() {
 
         //Top
         this.scene.pushMatrix()
-        this.scene.translate(0, 0, 0.5)
-        this.scene.rotate(270 * Math.PI / 180, 1, 0, 0)
+        this.scene.translate(0,0.5,0)
+        this.scene.rotate(3 * Math.PI/2 ,1, 0, 0)
         this.quad.display()
         this.scene.popMatrix()
 
-        //Back
-        this.scene.pushMatrix()
-        this.scene.translate(0, 0, -0.5)
-        this.scene.rotate(90 * Math.PI / 180, 1, 0, 0)
-        this.quad.display()
-        this.scene.popMatrix()
-
-        //Left
-        this.scene.pushMatrix()
-        this.scene.translate(-0.5, 0, 0)
-        this.scene.rotate(270 * Math.PI / 180, 0, 0, 1)
-        this.quad.display()
-        this.scene.popMatrix()
-
-        //Right
+        //Front
         this.scene.pushMatrix()
         this.scene.translate(0.5, 0, 0)
-        this.scene.rotate(90 * Math.PI / 180, 0, 0, 1)
+        this.scene.rotate(Math.PI / 2, 0, 1, 0)
         this.quad.display()
         this.scene.popMatrix()
-
-        //Top
+        
+        //Right
         this.scene.pushMatrix()
-        this.scene.translate(0,0.5,0)
-        this.scene.rotate( Math.PI , 1, 0, 0)
+        this.scene.translate(0,0,-0.5)
+        this.scene.rotate(Math.PI,0, 1, 0)
+        this.quad.display()
+        this.scene.popMatrix()
+        
+        //Back
+        this.scene.pushMatrix()
+        this.scene.translate(-0.5, 0, 0)
+        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0)
+        this.quad.display()
+        this.scene.popMatrix()
+        
+        //Left
+        this.scene.pushMatrix()
+        this.scene.translate(0,0,0.5)
         this.quad.display()
         this.scene.popMatrix()
 
         //Bottom
         this.scene.pushMatrix()
         this.scene.translate(0,-0.5,0)
+        this.scene.rotate(Math.PI/2 ,1, 0, 0)
         this.quad.display()
         this.scene.popMatrix()
-
-        
+    
     }
 
 
